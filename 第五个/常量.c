4210318f6b965d0c1ea6c4dda84c107330aef0dc
creatablr <zlj1542104994@gmail.com>
2021-12-09 23:55:49 +0800

    常量

---
+++ 第五个/常量.c
@@ -0,0 +1,11 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+int main() {
+
+//	const int num = 10;//num就是常变量-具有常属性
+	//num = 20;
+//	printf("num=%d\n",num);
+	//int arr[10] = { 0 };//10个元素
+	//const int n = 10;
+	//int arr2[n] = { 0 };
+	//return 0;
+}
